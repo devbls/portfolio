fb8a7f1ebbebcec570ed34fb6b75c2dca09bebef
Brenno Santos <devblsds@gmail.com>
2024-01-27 02:27:07 -0300

    Add links to technologies

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,40 +5,44 @@ export const Navbar = () => {
   const { t, i18n } = useTranslation("tabs");
 
   return (
-    <header className="flex justify-between items-center h-20 p-8">
-      <img className="h-12 w-12" src="/logo.png" alt="Logo" />
-      <nav className="flex gap-12">
-        <a href="#about">
+    <header className="w-full flex items-center h-20 bg-black-500 sticky -top-4 z-10 shadow-xl">
+      <div className="w-full flex justify-between items-center h-16 px-4 bg-black-500 sticky top-0">
+        <img className="h-12 w-12" src="/logo.png" alt="Logo" />
+        <nav className="flex gap-12">
+          <a href="#about">
+            <span className="font-poppins text-lg text-gray transition-all duration-40 hover:text-white hover:cursor-pointer">
+              {t("about")}
+            </span>
+          </a>
           <span className="font-poppins text-lg text-gray transition-all duration-40 hover:text-white hover:cursor-pointer">
-            {t("about")}
+            {t("projects")}
           </span>
-        </a>
-        <span className="font-poppins text-lg text-gray transition-all duration-40 hover:text-white hover:cursor-pointer">
-          {t("projects")}
-        </span>
-        <span className="font-poppins text-lg text-gray transition-all duration-40 hover:text-white hover:cursor-pointer">
-          {t("techs")}
-        </span>
-      </nav>
-      <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2 text-gray">
-          <button
-            className={`h-8 w-8 text-purple-500 font-poppins ${
-              i18next.resolvedLanguage === "pt" ? "font-bold" : "font-light"
-            } transition-all duration-40`}
-            onClick={() => i18n.changeLanguage("pt")}
-          >
-            PT
-          </button>
-          <div className="h-8 w-0.5 bg-purple-500" />
-          <button
-            className={`h-8 w-8 text-purple-500 font-poppins ${
-              i18next.resolvedLanguage === "en" ? "font-bold" : "font-light"
-            } transition-all duration-40`}
-            onClick={() => i18n.changeLanguage("en")}
-          >
-            EN
-          </button>
+          <a href="#techs">
+            <span className="font-poppins text-lg text-gray transition-all duration-40 hover:text-white hover:cursor-pointer">
+              {t("techs")}
+            </span>
+          </a>
+        </nav>
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2 text-gray">
+            <button
+              className={`h-8 w-8 text-purple-500 font-poppins ${
+                i18next.resolvedLanguage === "pt" ? "font-bold" : "font-light"
+              } transition-all duration-40`}
+              onClick={() => i18n.changeLanguage("pt")}
+            >
+              PT
+            </button>
+            <div className="h-8 w-0.5 bg-purple-500" />
+            <button
+              className={`h-8 w-8 text-purple-500 font-poppins ${
+                i18next.resolvedLanguage === "en" ? "font-bold" : "font-light"
+              } transition-all duration-40`}
+              onClick={() => i18n.changeLanguage("en")}
+            >
+              EN
+            </button>
+          </div>
         </div>
       </div>
     </header>
--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -21,75 +21,95 @@ const TECHNOLOGIES = [
   {
     name: "React",
     icon: <FaReact size={56} />,
+    link: "https://react.dev",
   },
   {
     name: "HTML",
     icon: <FaHtml5 size={56} />,
+    link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML",
   },
   {
     name: "CSS",
     icon: <FaCss3Alt size={56} />,
+    link: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
   },
   {
     name: "JavaScript",
     icon: <SiJavascript size={56} />,
+    link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
   },
   {
     name: "TypeScript",
     icon: <SiTypescript size={56} />,
+    link: "https://www.typescriptlang.org",
   },
   {
     name: "Next.js",
     icon: <TbBrandNextjs size={56} />,
+    link: "https://nextjs.org",
   },
   {
     name: "React Native",
     icon: <TbBrandReactNative size={56} />,
+    link: "https://reactnative.dev",
   },
   {
     name: "Git",
     icon: <FaGitAlt size={56} />,
+    link: "https://git-scm.com",
   },
   {
     name: "Tailwind CSS",
     icon: <SiTailwindcss size={56} />,
+    link: "https://tailwindcss.com",
   },
   {
     name: "Chakra UI",
     icon: <SiChakraui size={56} />,
+    link: "https://chakra-ui.com",
   },
   {
     name: "Redux",
     icon: <SiRedux size={56} />,
+    link: "https://redux.js.org",
   },
   {
     name: "React Query",
     icon: <SiReactquery size={56} />,
+    link: "https://tanstack.com/query/latest/docs/framework/react/overview",
   },
   {
     name: "Strapi",
     icon: <SiStrapi size={56} />,
+    link: "https://strapi.io",
   },
   {
     name: "Nodejs",
     icon: <FaNodeJs size={56} />,
+    link: "https://nodejs.org/en",
   },
   {
     name: "Scrum",
     icon: <DiScrum size={56} />,
+    link: "https://www.scrum.org",
   },
 ];
 
 export const Technologies = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[120vh]">
+    <div
+      id="techs"
+      className="flex flex-col items-center justify-center min-h-[120vh]"
+    >
       <h2 className="font-dm text-3xl font-bold text-zinc-50">Tecnologias</h2>
       <div className="max-w-4xl grid grid-cols-[repeat(auto-fill,_minmax(144px,_1fr))] gap-6 mt-8">
         {TECHNOLOGIES.map((tech) => (
-          <div className="flex flex-col gap-4 items-center justify-center h-36 w-36 bg-black-400 text-purple-500 rounded-md border border-stone-700 hover:border-purple-500 transition-all duration-300">
-            <p className="font-poppins text-md mt-4">{tech.name}</p>
-            <div className="mb-auto">{tech.icon}</div>
-          </div>
+          <a href={tech.link} target="_blank">
+            <div className="flex flex-col gap-4 items-center justify-center h-36 w-36 bg-black-400 text-purple-500 rounded-md border border-stone-700 hover:border-purple-500 transition-all duration-300">
+              <p className="font-poppins text-md mt-4">{tech.name}</p>
+              <div className="mb-auto">{tech.icon}</div>
+            </div>
+          </a>
         ))}
       </div>
     </div>
