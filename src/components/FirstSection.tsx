2c41f6d180f3f435e80ff941db45ce014d9bc440
Brenno Santos <devblsds@gmail.com>
2024-01-27 14:06:10 -0300

    Add social links to first section

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -1,3 +1,6 @@
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+
 import cv from "../assets/CVBrennoSantos.pdf";
 
 export const FirstSection = () => {
@@ -12,11 +15,33 @@ export const FirstSection = () => {
           <p className="font-poppins text-xl text-zinc-500">
             Sou desenvolvedor Frontend
           </p>
-          <button className="h-8 w-36 mt-4 text-sm font-dm bg-purple-500 hover:bg-purple-600 text-zinc-200 rounded-full transition">
-            <a href={cv} download="CV" target="_blank">
-              Download CV
-            </a>
-          </button>
+          <div className="flex items-center mt-4 gap-6">
+            <button className="h-8 w-36 text-sm font-dm bg-purple-500 hover:bg-purple-600 text-zinc-200 rounded-full transition">
+              <a href={cv} download="CV" target="_blank">
+                Download CV
+              </a>
+            </button>
+            <div className="flex gap-3">
+              <a
+                href="https://www.linkedin.com/in/brenno-luis/"
+                target="_blank"
+              >
+                <div className="p-2 text-purple-500 border border-stone-600 rounded-md hover:border-purple-500 transition-all duration-300">
+                  <FaLinkedinIn size={18} />
+                </div>
+              </a>
+              <a href="https://github.com/devbls" target="_blank">
+                <div className="p-2 text-purple-500 border border-stone-600 rounded-md hover:border-purple-500 transition-all duration-300">
+                  <FaGithub size={18} />
+                </div>
+              </a>
+              <a href="mailto:devblsds@gmail.com" target="_blank">
+                <div className="p-2 text-purple-500 border border-stone-600 rounded-md hover:border-purple-500 transition-all duration-300">
+                  <BiLogoGmail size={18} />
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
         <img className="h-128 w-128" src="/logo.webp" alt="Logo" />
       </div>
