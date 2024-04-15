d29d1a265fb1ed1c7afa714365fd13127b517434
Brenno Santos <devblsds@gmail.com>
2024-04-15 19:32:02 -0300

    Refactor Google Analytics implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ReactGA from "react-ga";
 import "@fontsource/poppins";
 import "@fontsource-variable/dm-sans";
 
 import "./index.css";
 import App from "./App.tsx";
-
-const TRACKING_ID = "G-7F260S81M5";
-ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
