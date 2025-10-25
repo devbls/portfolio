e42f28667187b34d8189fbcf89f7e071f8083274
Brenno Santos <devblsds@gmail.com>
2025-10-25 12:38:21 -0300

    Adjust thumb size

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-    target: 'esnext',
-  },
+  plugins: [react()]
 });
